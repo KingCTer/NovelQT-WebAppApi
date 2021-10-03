d610177676bbd099094c4d5805671f29b7bf74d7
KingCTer <TriDung.AK54@outlook.com.vn>
2021-10-03 12:31:14 +0700

    Add Migrations

---
+++ WebApi/src/NovelQT.Services.Api/Controllers/BookController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NovelQT.Services.Api.Controllers
+{
+    public class BookController : PublicApiController
+    {
+        [HttpGet]
+        public ActionResult<string> TestApi()
+        {
+            return "Ok";
+        }
+    }
+}
